b9d39e533da30bb127c1122148ce4bd072bc8e01
JaeSeong Song <zzyzigy@naver.com>
2023-06-26 14:41:08 +0900

    logging 사용하기

--- src/main/java/zzyzzy/hello/spring4/controller/IndexController.java
+++ src/main/java/zzyzzy/hello/spring4/controller/IndexController.java
@@ -1,5 +1,7 @@
 package zzyzzy.hello.spring4.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,13 +9,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    // 로거 설정 : getLogger(로깅할클래스명)
+    private Logger logger = LogManager.getLogger(IndexController.class);
+
     // http://localhost:8080/ 호출시 실행
     @RequestMapping("/")
     public String index(Model m) {
 
         // addAttribute(변수명, 대상객체)
         m.addAttribute("sayHello", "Hello World! from Controller");
-        
+
+        // 로거 출력
+        logger.info("Hello, World!! from Logger!");
+
         return "index";
     }
 
