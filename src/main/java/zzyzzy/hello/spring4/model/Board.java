746bb78976c8a3ee1a568fff05ac5648075e8b18
JaeSeong Song <zzyzigy@naver.com>
2023-06-26 17:40:39 +0900

    DTO/VO 클래스에 lombok 적용

--- src/main/java/zzyzzy/hello/spring4/model/Board.java
+++ src/main/java/zzyzzy/hello/spring4/model/Board.java
@@ -1,5 +1,14 @@
 package zzyzzy.hello.spring4.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@ToString
 public class Board {
 
     private String bno;
@@ -8,74 +17,5 @@ public class Board {
     private String regdate;
     private String views;
     private String contents;
-
-    public Board(String bno, String title, String userid, String regdate, String views, String contents) {
-        this.bno = bno;
-        this.title = title;
-        this.userid = userid;
-        this.regdate = regdate;
-        this.views = views;
-        this.contents = contents;
-    }
-
-    public String getBno() {
-        return bno;
-    }
-
-    public void setBno(String bno) {
-        this.bno = bno;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getUserid() {
-        return userid;
-    }
-
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-
-    public String getRegdate() {
-        return regdate;
-    }
-
-    public void setRegdate(String regdate) {
-        this.regdate = regdate;
-    }
-
-    public String getViews() {
-        return views;
-    }
-
-    public void setViews(String views) {
-        this.views = views;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public void setContents(String contents) {
-        this.contents = contents;
-    }
-
-    @Override
-    public String toString() {
-        return "Board{" +
-                "bno='" + bno + '\'' +
-                ", title='" + title + '\'' +
-                ", userid='" + userid + '\'' +
-                ", regdate='" + regdate + '\'' +
-                ", views='" + views + '\'' +
-                ", contents='" + contents + '\'' +
-                '}';
-    }
 
 }
--- src/main/java/zzyzzy/hello/spring4/model/Member.java
+++ src/main/java/zzyzzy/hello/spring4/model/Member.java
@@ -1,5 +1,14 @@
 package zzyzzy.hello.spring4.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@ToString
 public class Member {
 
     private String mno;
@@ -8,74 +17,5 @@ public class Member {
     private String name;
     private String email;
     private String regdate;
-
-    public Member(String mno, String userid, String passwd, String name, String email, String regdate) {
-        this.mno = mno;
-        this.userid = userid;
-        this.passwd = passwd;
-        this.name = name;
-        this.email = email;
-        this.regdate = regdate;
-    }
-
-    public String getMno() {
-        return mno;
-    }
-
-    public void setMno(String mno) {
-        this.mno = mno;
-    }
-
-    public String getUserid() {
-        return userid;
-    }
-
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-
-    public String getPasswd() {
-        return passwd;
-    }
-
-    public void setPasswd(String passwd) {
-        this.passwd = passwd;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRegdate() {
-        return regdate;
-    }
-
-    public void setRegdate(String regdate) {
-        this.regdate = regdate;
-    }
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "mno='" + mno + '\'' +
-                ", userid='" + userid + '\'' +
-                ", passwd='" + passwd + '\'' +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", regdate='" + regdate + '\'' +
-                '}';
-    }
 
 }
