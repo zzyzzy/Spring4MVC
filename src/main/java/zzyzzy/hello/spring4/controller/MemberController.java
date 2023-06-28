518bef426555ca9ef3949f7bc2842dc814e2a6cc
JaeSeong Song <zzyzigy@naver.com>
2023-06-28 14:58:08 +0900

    join/login 요청 메서드 추가

--- src/main/java/zzyzzy/hello/spring4/controller/MemberController.java
+++ src/main/java/zzyzzy/hello/spring4/controller/MemberController.java
@@ -5,13 +5,14 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MemberController {
 
     private Logger logger = LogManager.getLogger(MemberController.class);
 
-    @RequestMapping("/member/join")
+    @RequestMapping(value = "/member/join", method = RequestMethod.GET)
     public String join(Model m) {
 
         logger.info("member/join 호출!");
@@ -19,12 +20,28 @@ public class MemberController {
         return "member/join.tiles";
     }
 
-    @RequestMapping("/member/login")
+    @RequestMapping(value = "/member/join", method = RequestMethod.POST)
+    public String joinok(Model m) {
+
+        logger.info("member/joinok 호출!");
+
+        return "redirect:/member/login";
+    }
+
+    @RequestMapping(value = "/member/login", method = RequestMethod.GET)
     public String login(Model m) {
 
         logger.info("member/login 호출!");
 
         return "member/login.tiles";
+    }
+
+    @RequestMapping(value = "/member/login", method = RequestMethod.POST)
+    public String loginok(Model m) {
+
+        logger.info("member/loginok 호출!");
+
+        return "redirect:/member/myinfo";
     }
 
     @RequestMapping("/member/myinfo")
