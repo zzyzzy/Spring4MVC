2f2aed98690711969abe094e91fde50f8e213022
JaeSeong Song <zzyzigy@naver.com>
2023-06-29 15:17:21 +0900

    MemberService loginMember 구현

--- src/main/java/zzyzzy/hello/spring4/service/MemberService.java
+++ src/main/java/zzyzzy/hello/spring4/service/MemberService.java
@@ -5,5 +5,6 @@ import zzyzzy.hello.spring4.model.Member;
 public interface MemberService {
 
     boolean saveMember(Member m);
+    boolean loginMember(Member m);
 
 }
--- src/main/java/zzyzzy/hello/spring4/service/MemberServiceImpl.java
+++ src/main/java/zzyzzy/hello/spring4/service/MemberServiceImpl.java
@@ -19,4 +19,15 @@ public class MemberServiceImpl implements MemberService {
         return isSaved;
     }
 
+    @Override
+    public boolean loginMember(Member m) {
+        boolean isLogin = false;
+
+        if (mdao.loginMember(m) != null)
+            isLogin = true;
+
+        return isLogin;
+    }
+
+
 }
