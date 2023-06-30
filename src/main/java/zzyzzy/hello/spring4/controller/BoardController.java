e959fc59b1ff32ef4a2bf84d8938493ec30ae7c1
JaeSeong Song <zzyzigy@naver.com>
2023-06-30 16:55:01 +0900

    BoardController : list 구현

--- src/main/java/zzyzzy/hello/spring4/controller/BoardController.java
+++ src/main/java/zzyzzy/hello/spring4/controller/BoardController.java
@@ -2,18 +2,26 @@ package zzyzzy.hello.spring4.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import zzyzzy.hello.spring4.service.BoardService;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
 
+    @Autowired BoardService bsrv;
+
     private Logger logger = LogManager.getLogger(BoardController.class);
 
     @GetMapping("/list")
-    public String list() {
+    public String list(Model m, int cpg) {
+        logger.info("board/list 호출!!");
+
+        m.addAttribute("boards", bsrv.readBoard(cpg));
 
         return "board/list.tiles";
     }
