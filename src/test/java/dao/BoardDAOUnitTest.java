b9f93bc873aad345ec3d9986e5feb7271f5b664d
JaeSeong Song <zzyzigy@naver.com>
2023-06-30 16:52:49 +0900

    BoardDAO : selectBoard 단위 테스트

---
+++ src/test/java/dao/BoardDAOUnitTest.java
@@ -0,0 +1,46 @@
+package dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import zzyzzy.hello.spring4.dao.BoardDAO;
+import zzyzzy.hello.spring4.dao.MemberDAO;
+import zzyzzy.hello.spring4.model.Board;
+import zzyzzy.hello.spring4.model.Member;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/servlet-context.xml",
+    "classpath:spring/root-context.xml"})
+@WebAppConfiguration
+public class BoardDAOUnitTest {
+
+    @Autowired
+    BoardDAO bdao;
+
+    @Test
+    public void selectBoard() throws Exception {
+        int cpage = 3;
+        int snum = (cpage - 1) * 15;
+        List<Board> results = bdao.selectBoard(snum);
+
+        assertEquals(results.size(), 15);
+        System.out.println(results);
+    }
+
+}
+
+
+
+
+
+
+
