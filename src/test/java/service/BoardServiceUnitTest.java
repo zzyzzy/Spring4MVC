d6203db9d9f47448c6ce6bae803fdfe6dc3e765f
JaeSeong Song <zzyzigy@naver.com>
2023-06-30 16:54:01 +0900

    BoardService : readBoard 단위테스트

---
+++ src/test/java/service/BoardServiceUnitTest.java
@@ -0,0 +1,45 @@
+package service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+import zzyzzy.hello.spring4.model.Board;
+import zzyzzy.hello.spring4.model.Member;
+import zzyzzy.hello.spring4.service.BoardService;
+import zzyzzy.hello.spring4.service.MemberService;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/servlet-context.xml",
+    "classpath:spring/root-context.xml"})
+@WebAppConfiguration
+public class BoardServiceUnitTest {
+
+    // 테스트할 대상 객체를 주입받음
+    @Autowired BoardService bsrv;
+
+    @Test
+    public void readBoard() throws Exception {
+        int cpage = 1;
+        List<Board> results = bsrv.readBoard(cpage);
+
+        assertEquals(results.size(), 15);
+        System.out.println(results);
+    }
+
+}
+
+
+
+
+
+
+
