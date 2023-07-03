b41fc4c872142ec35ad84ead9adaad6efca5b5b4
JaeSeong Song <zzyzigy@naver.com>
2023-07-03 16:59:41 +0900

    join에 대해 포인트컷, 어드바이스 추가

--- src/main/java/zzyzzy/hello/spring4/aop/MemberServiceAdvice.java
+++ src/main/java/zzyzzy/hello/spring4/aop/MemberServiceAdvice.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpSession;
@@ -15,8 +16,13 @@ public class MemberServiceAdvice {
 
     private Logger logger = LogManager.getLogger(MemberServiceAdvice.class);
 
+    @Pointcut("execution(* zzyzzy.hello.spring4.controller.MemberController.myinfo(..))")
+    public void myInfoPoint() {}
 
-    @Around("execution(* zzyzzy.hello.spring4.controller.MemberController.myinfo(..))")
+    @Pointcut("execution(* zzyzzy.hello.spring4.controller.MemberController.join(..))")
+    public void joinPoint() {}
+
+    @Around("myInfoPoint()")
     public Object myinfoAOPProcess(ProceedingJoinPoint pjp)
                                                 throws Throwable {
         logger.info("myinfoAOPProcess 호출!!");
@@ -31,6 +37,27 @@ public class MemberServiceAdvice {
         // 포인트컷 대상 메서드 실행
         if (sess.getAttribute("member") == null)
             return "redirect:/member/login";
+
+        //
+        Object obj = pjp.proceed();
+        return obj;
+    }
+
+    @Around("joinPoint()")
+    public Object joinAOPProcess(ProceedingJoinPoint pjp) throws Throwable {
+        logger.info("joinAOPProcess 호출!!");
+        HttpSession sess = null;
+
+        for(Object o : pjp.getArgs()) {
+            if (o instanceof HttpSession)
+                sess = (HttpSession) o;
+        }
+
+        // 포인트컷 대상 메서드 실행
+        // 로그인 안 했다면 -> join으로 이동
+        // 로그인 했다면 -> myinfo로 이동
+        if (sess.getAttribute("member") != null)
+            return "redirect:/member/myinfo";
 
         //
         Object obj = pjp.proceed();
