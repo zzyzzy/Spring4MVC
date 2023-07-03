2fe5d810d9ff7716c215da1577f914b9e6fda6c1
JaeSeong Song <zzyzigy@naver.com>
2023-07-03 11:13:04 +0900

    boardService : readOneBoard 단위테스트

--- src/test/java/service/BoardServiceUnitTest.java
+++ src/test/java/service/BoardServiceUnitTest.java
@@ -35,6 +35,15 @@ public class BoardServiceUnitTest {
         System.out.println(results);
     }
 
+    @Test
+    public void readOneBoard() throws Exception {
+        String bno = "450";
+        Board result = bsrv.readOneBoard(bno);
+
+        assertNotNull(result);
+        System.out.println(result);
+    }
+
 }
 
 
