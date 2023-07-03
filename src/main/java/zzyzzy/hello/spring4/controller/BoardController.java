68912ccf658b0308d31e3acd9f248f614767b576
JaeSeong Song <zzyzigy@naver.com>
2023-07-03 14:54:02 +0900

    boardController : writeok 구현

--- src/main/java/zzyzzy/hello/spring4/controller/BoardController.java
+++ src/main/java/zzyzzy/hello/spring4/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import zzyzzy.hello.spring4.model.Board;
 import zzyzzy.hello.spring4.service.BoardService;
@@ -31,6 +32,16 @@ public class BoardController {
     public String write() {
 
         return "board/write.tiles";
+    }
+
+    @PostMapping ("/write")
+    public String writeok(Board bd) {
+        String returnPage = "redirect:/board/fail";
+
+        if (bsrv.saveBoard(bd))
+            returnPage = "redirect:/board/list?cpg=1";
+
+        return returnPage;
     }
 
     @GetMapping("/view")
