ca1ba24d0af0e668f131009041b1450a4861b2fc
JaeSeong Song <zzyzigy@naver.com>
2023-07-03 14:52:19 +0900

    boardDAO : insertBoard 단위테스트

--- src/test/java/dao/BoardDAOUnitTest.java
+++ src/test/java/dao/BoardDAOUnitTest.java
@@ -45,6 +45,15 @@ public class BoardDAOUnitTest {
         System.out.println(result);
     }
 
+    @Test
+    @Transactional
+    public void insertBoard() throws Exception {
+        Board bd = new Board(null, "테스트", "abc123",
+                null, null, "냉무");
+
+        assertEquals(bdao.insertBoard(bd), 1);
+    }
+
 }
 
 
