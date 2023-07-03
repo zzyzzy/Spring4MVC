558f35adeca9abb3de370eead0305a99cb97e797
JaeSeong Song <zzyzigy@naver.com>
2023-07-03 14:54:57 +0900

    boardController : writeok 단위테스트

--- src/test/java/controller/BoardControllerUnitTest.java
+++ src/test/java/controller/BoardControllerUnitTest.java
@@ -57,4 +57,17 @@ public class BoardControllerUnitTest {
         System.out.println(mvcResult.getModelAndView());
     }
 
+    @Test
+    @Transactional
+    public void writeokTest() throws Exception {
+        mockMvc.perform(post("/board/write")
+                .param("title", "테스트")
+                .param("userid", "abc123")
+                .param("contents", "냉무"))
+                .andExpect(status().is3xxRedirection())
+                //.andExpect(view().name("redirect:/board/list"));
+                .andExpect(redirectedUrl("/board/list"));
+
+    }
+
 }
